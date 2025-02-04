112f5b39dd8fb0b8bbb18c37a4e9e2cd4f5b11c7
Lea Cotar <lea.cotar@gmx.ch>
2025-02-04 18:34:58 +0100

    upload

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-dialog-add-player',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrl: './dialog-add-player.component.scss',
+})
+export class DialogAddPlayerComponent {
+  name: string = '';
+  constructor() {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+
+  onNoCLick() {}
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
+import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { PlayerComponent } from '../player/player.component';
+import { FormsModule } from '@angular/forms';
+// import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatIconModule,
+    PlayerComponent,
+    FormsModule,
+  ],
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
-  pickCardAnimation = false;
+  takeCardAnimation = false;
+  currentCard: string = '';
   game: Game = new Game();
+  card: any;
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -26,6 +41,30 @@ export class GameComponent {
   }
 
   pickCard() {
-    this.pickCardAnimation = true;
+    if (!this.takeCardAnimation) {
+      this.takeCardAnimation = true;
+      let popCard = this.game.stack.pop();
+
+      if (popCard !== undefined) {
+        this.currentCard = popCard;
+      }
+      /* this.updateGame(); */
+      this.game.currentPlayer++;
+      this.game.currentPlayer =
+        this.game.currentPlayer % this.game.players.length;
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.takeCardAnimation = false;
+        /* this.updateGame(); */
+      }, 1000);
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name) => {
+      this.game.players.push(name);
+    });
   }
 }
